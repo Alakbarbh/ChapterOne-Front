9934b7e735bf6a06d7558bbaf23c0d6cb156b408
Alakbarbh <alakbarbh@code.edu.az>
2023-06-02 09:36:23 +0400

    ChapterOne Start

---
+++ assets/js/home.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    let search = document.querySelector(".search i")
+    search.addEventListener("click", function () {
+        document.querySelector(".search-input").classList.toggle("d-none")
+    })
+
+    let login = document.querySelector(".user i")
+    login.addEventListener("click", function () {
+        document.querySelector(".login-register").classList.toggle("d-none")
+    })
+
+
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+    //login-registerdə body-ə vuranda işləməsi üçün js
+    document.addEventListener("click", function (e) {
+        if (!!!e.target.closest(".user")) {
+            if (!$(".login-register").hasClass("d-none")) {
+                $(".login-register").addClass("d-none")
+            }
+        }
+    })
+
+
+    // let headers = document.querySelectorAll(".category .item");
+
+    // let contents = document.querySelectorAll("#products .products")
+
+
+    // headers.forEach(element => {
+    //     element.addEventListener("click", function () {
+    //         document.querySelector(".active").classList.remove("active");
+    //         this.classList.add("active");
+
+    //         contents.forEach(content => {
+    //             if (content.getAttribute("data-id") == this.getAttribute("data-id")) {
+    //                 content.classList.remove("d-none")
+    //             } else {
+    //                 content.classList.add("d-none")
+    //             }
+
+    //         });
+    //     })
+    // });
+
+
+    
+})
